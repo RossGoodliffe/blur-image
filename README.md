c05cfa25cbf4513c75776aefe208c191f0831b4b
Ross <rossgoodliffe@gmail.com>
2022-02-01 22:32:02 +0000

    base files and functionality added

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# blur-image
+# Blur Image
+
+Basic HTML/CSS/JS webpage that loads a blured image and slowly unblurs the image
---
+++ app.js
@@ -0,0 +1,30 @@
+
+// HTML classes
+const loadText = document.querySelector('.loading');
+const background = document.querySelector('.background');
+
+// Load status value
+let load = 0;
+
+// Interval the function will run
+let int = setInterval(blurring, 30);
+
+function blurring() {
+    load++;
+
+        // reset load value if over 99
+        if(load > 99) {
+            clearInterval(int);
+        }
+
+    loadText.innerText = `${load}%`
+    loadText.style.opacity = scale(load, 0, 100, 1, 0);
+    background.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`
+}
+
+// map a range of numbers to another range of numbers
+// https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
+function scale (number, inMin, inMax, outMin, outMax) {
+    return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+}
+
